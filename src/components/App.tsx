ab7b876b7c180be3d0c0346c5f6d6c5199fa406c
Serhii Rubtsov <rubtsov1986@bigmir.net>
2022-11-19 22:20:58 +0200

    reload files

---
+++ src/components/App.tsx
@@ -0,0 +1,18 @@
+export const App: React.FC = () => {
+  return (
+
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        textTransform: 'uppercase',
+        color: '#010101',
+      }}
+    >
+      React homework template1
+    </div>
+  );
+};
